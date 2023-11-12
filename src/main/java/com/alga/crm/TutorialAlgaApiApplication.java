6feae29873f8d352b622d397920e0d76d8237368
Giovanni Torelli <giovanni.walter@outlook.com>
2023-11-12 17:46:22 -0300

    initial commit

---
+++ src/main/java/com/alga/crm/TutorialAlgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.alga.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAlgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAlgaApiApplication.class, args);
+	}
+
+}
