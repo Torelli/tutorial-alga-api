6103195541fe05db01c3fa978db5274aa82fa79a
Giovanni Torelli <giovanni.walter@outlook.com>
2023-11-12 18:11:47 -0300

    Created Cliente model

--- src/main/java/com/alga/crm/controller/ClienteController.java
+++ src/main/java/com/alga/crm/controller/ClienteController.java
@@ -1,2 +1,18 @@
-package com.alga.crm.controller;public class ClienteController {
+package com.alga.crm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @GetMapping
+    public List<Cliente> showClients() {
+
+    }
+
 }
---
+++ src/main/java/com/alga/crm/model/Cliente.java
@@ -0,0 +1,4 @@
+package com.alga.crm.model;
+
+public class Cliente {
+}
