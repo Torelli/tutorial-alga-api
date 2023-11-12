e82a8dde29eeb02162660a9fec390be39607a1d0
Giovanni Torelli <giovanni.walter@outlook.com>
2023-11-12 18:28:28 -0300

    Created Cliente model

--- src/main/java/com/alga/crm/model/Cliente.java
+++ src/main/java/com/alga/crm/model/Cliente.java
@@ -1,4 +1,17 @@
 package com.alga.crm.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
 }
